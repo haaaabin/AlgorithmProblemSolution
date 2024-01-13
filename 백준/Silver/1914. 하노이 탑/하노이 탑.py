4089c189b33c27811d1aa3ee2c4f779ba88f1828
choihabin <154362206+haaaabin@users.noreply.github.com>
2024-01-13 17:29:15 +0900

    [Silver I] Title: 하노이 탑, Time: 888 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1914. 하노이 탑/하노이 탑.py
@@ -0,0 +1,12 @@
+def hanoi(n,x,y):
+    if n > 1:
+        hanoi(n-1,x,6-x-y)
+    print(x,y)
+    
+    if n > 1:
+        hanoi(n-1,6-x-y,y)
+        
+n = int(input())
+print(2**n-1)
+if n <= 20:
+    hanoi(n,1,3)
