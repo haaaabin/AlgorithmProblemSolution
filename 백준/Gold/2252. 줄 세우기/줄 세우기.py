d7878c2cd3ceaa01a0a42b3be3a3e6c99823cab1
choihabin <154362206+haaaabin@users.noreply.github.com>
2024-01-24 12:38:46 +0900

    [Gold III] Title: 줄 세우기, Time: 4116 ms, Memory: 38380 KB -BaekjoonHub

---
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.py
@@ -0,0 +1,33 @@
+from collections import deque
+
+N,M = map(int,input().split())
+
+student = [[] for _ in range(N+1)]
+
+indegree = [0] * (N+1)
+
+for i in range(M):
+    a,b = map(int,input().split())
+    student[a].append(b)
+    indegree[b] +=1
+    
+def toplogy_sort():
+    q = deque()
+    result = []
+    #진입 차수 0인 노드 큐에 추가
+    for i in range(1,N+1):
+        if indegree[i] == 0:
+            q.append(i)
+            
+    while q:
+        p = q.popleft()
+        result.append(p)
+        for i in student[p]:
+            indegree[i] -=1
+            if indegree[i] == 0:
+                q.append(i)
+                
+    for res in result:
+        print(res, end=" ")    
+        
+toplogy_sort()
