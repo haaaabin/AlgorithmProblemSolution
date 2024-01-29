9c337769e06291ce155a0872c1cdfbabcae9120c
choihabin <154362206+haaaabin@users.noreply.github.com>
2024-01-29 16:34:04 +0900

    [Gold IV] Title: LCS 2, Time: 560 ms, Memory: 55708 KB -BaekjoonHub

--- 백준/Gold/9252. LCS 2/LCS 2.py
+++ 백준/Gold/9252. LCS 2/LCS 2.py
@@ -1,21 +1,23 @@
 import sys
 input = sys.stdin.readline
-
-string_A = [""]+list(input().rstrip())
-string_B = [""]+list(input().rstrip())
-
-lcs = [[""] * (len(string_B)) for _ in range(len(string_A))]
-
-
-for i in range(1,len(string_A)):
-    for j in range(1,len(string_B)):
-        if string_A[i] == string_B[j]:
-            lcs[i][j] = lcs[i-1][j-1] + string_A[i]
+A,B = "-"+input().rstrip(),"-"+input().rstrip()
+dp = [[0]*(len(A)) for _ in range(len(B))]
+for i in range(1,len(B)):
+    for j in range(1,len(A)):
+        if A[j] == B[i]:
+            dp[i][j] = dp[i-1][j-1]+1
         else:
-            if len(lcs[i-1][j]) >= len(lcs[i][j-1]):
-                lcs[i][j] = lcs[i-1][j]
-            else:
-                lcs[i][j] = lcs[i][j-1]
-
-result = lcs[-1][-1]
-print(len(result), result , sep ="\n")
+            dp[i][j] = max(dp[i][j-1],dp[i-1][j])
+print(dp[-1][-1])
+i,j = len(B)-1,len(A)-1
+ans = ""
+while i>0 and j>0:
+    if dp[i][j] == dp[i][j-1]:
+        j-=1
+    elif dp[i][j] == dp[i-1][j]:
+        i-=1
+    else:
+        ans = A[j] + ans
+        i-=1; j-=1
+if ans:
+    print(ans)
