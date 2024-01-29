eb58ee6335d869f54d49fee67d4283cfc6197a86
choihabin <154362206+haaaabin@users.noreply.github.com>
2024-01-29 23:56:59 +0900

    [Silver I] Title: 신입 사원, Time: 2868 ms, Memory: 216156 KB -BaekjoonHub

--- 백준/Silver/1946. 신입 사원/신입 사원.py
+++ 백준/Silver/1946. 신입 사원/신입 사원.py
@@ -1,11 +1,16 @@
+import sys
+input = sys.stdin.readline
+
 T = int(input().strip())
 
 for _ in range(T):
     N = int(input().strip())
     applicants = [tuple(map(int, input().split())) for _ in range(N)]
     
+    # 서류 심사 성적을 기준으로 정렬
     applicants.sort(key=lambda x: x[0])
 
+    # 면접 성적 비교
     max_rank = applicants[0][1]
     count = 1
     for i in range(1, N):
