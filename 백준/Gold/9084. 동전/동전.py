51701b370b90bce68bcea23b8fe69d073bd80ad8
choihabin <154362206+haaaabin@users.noreply.github.com>
2024-01-30 21:24:11 +0900

    [Gold V] Title: 동전, Time: 84 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/9084. 동전/동전.py
+++ 백준/Gold/9084. 동전/동전.py
@@ -5,21 +5,15 @@ T = int(input())
 
 for _ in range(T):
     n = int(input())
-    coins = list(map(int,input().split()))
-    coins.insert(0,0)
-    amount = int(input())
+    coins = list(map(int, input().split()))
+    m = int(input())
     
-    dp = [[0] *(amount+1) for i in range(n+1)]
+    dp=[0] * (m+1)
+    dp[0] = 1
     
-    for i in range(n+1):
-        #0원으로 만들 수 있는 가지 수는 1로 초기화
-        dp[i][0] = 1
-            
-    for j in range(1,n+1):
-        for i in range(1,amount+1):
-            #앞선 coin의 가지수를 다음 coins 칸에 가지고 온다
-            dp[j][i] = dp[j-1][i]
-            #만들고 싶은 값에서 내가 지금 가진 동전을 뺀다.
-            if i-coins[j] >= 0:
-                dp[j][i] += dp[j][i-coins[j]]
-    print(dp[n][amount])
+    for coin in coins:
+        for i in range(1, m+1):
+            if i >= coin:
+                dp[i] += dp[i-coin]
+
+    print(dp[m])
