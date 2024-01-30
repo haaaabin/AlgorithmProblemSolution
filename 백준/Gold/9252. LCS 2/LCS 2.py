77b00bd2c19724774026b50a9eea08aea3d94470
choihabin <154362206+haaaabin@users.noreply.github.com>
2024-01-30 11:21:35 +0900

    [Gold IV] Title: LCS 2, Time: 532 ms, Memory: 55708 KB -BaekjoonHub

--- 백준/Gold/9252. LCS 2/LCS 2.py
+++ 백준/Gold/9252. LCS 2/LCS 2.py
@@ -1,23 +1,34 @@
 import sys
 input = sys.stdin.readline
-A,B = "-"+input().rstrip(),"-"+input().rstrip()
-dp = [[0]*(len(A)) for _ in range(len(B))]
+
+A ="-" + input().rstrip()
+B ="-" + input().rstrip()
+
+dp =[[0] * (len(A)) for i in range(len(B))]
+
 for i in range(1,len(B)):
     for j in range(1,len(A)):
-        if A[j] == B[i]:
+        if A[j] ==B[i]:
             dp[i][j] = dp[i-1][j-1]+1
         else:
-            dp[i][j] = max(dp[i][j-1],dp[i-1][j])
+            dp[i][j] = max(dp[i][j-1], dp[i-1][j])
+            
 print(dp[-1][-1])
-i,j = len(B)-1,len(A)-1
-ans = ""
-while i>0 and j>0:
-    if dp[i][j] == dp[i][j-1]:
-        j-=1
-    elif dp[i][j] == dp[i-1][j]:
-        i-=1
-    else:
-        ans = A[j] + ans
-        i-=1; j-=1
+
+i = len(B)-1
+j = len(A)-1
+
+ans=""
+
+while i >0 and j >0:
+   if dp[i][j] == dp[i][j-1]:
+       j -= 1
+   elif dp[i][j] == dp[i-1][j]:
+       i -=1
+   else:
+       ans = A[j] + ans
+       i-=1
+       j -=1
+
 if ans:
     print(ans)
