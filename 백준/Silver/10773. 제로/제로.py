ab33615cbfbbadfade7a57ce83d79b10c1ac4a1a
choihabin <154362206+haaaabin@users.noreply.github.com>
2024-02-12 19:25:48 +0900

    [Silver IV] Title: 제로, Time: 80 ms, Memory: 31900 KB -BaekjoonHub

---
+++ 백준/Silver/10773. 제로/제로.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+K =int(input())
+stack = []
+
+for _ in range(K):
+    temp = int(input())
+    if(temp == 0):
+        stack.pop()
+    else:
+        stack.append(temp)
+        
+print(sum(stack))  
