94d18bc2bef2355a88b4d778cd8a378ed97d075e
choihabin <154362206+haaaabin@users.noreply.github.com>
2024-02-16 13:42:56 +0900

    [Silver IV] Title: 암기왕, Time: 1492 ms, Memory: 187752 KB -BaekjoonHub

--- 백준/Silver IV/2776. 암기왕/암기왕.py
+++ 백준/Silver IV/2776. 암기왕/암기왕.py
@@ -1,33 +1,16 @@
 import sys
 input = sys.stdin.readline
 
-def binary_search(target, data):
-    start = 0
-    end = len(target) -1
-
-    while True:
-        if start > end:
-            return -1
-        
-        mid = (start + end) //2
-        if target[mid] < data:
-            start = mid + 1
-        elif target[mid] > data:
-            end = mid - 1
-        else:
-            return mid
-
-T =int(input())
+T = int(input())
 
 for i in range(T):
-    N = int(input())
-    note1 = list(map(int,input().split()))
-    M = int(input())
-    note2 = list(map(int,input().split()))
-    note1.sort()
+    n = int(input())
+    note1  = set(map(int,input().split()))
+    m = int(input())
+    note2 = map(int,input().split())
     
     for num in note2:
-        if binary_search(note1, num) >= 0:
+        if num in note1:
             print(1)
         else:
             print(0)
