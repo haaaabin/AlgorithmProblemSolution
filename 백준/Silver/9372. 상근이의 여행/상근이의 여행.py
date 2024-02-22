0d36a907fa4340b49da0f6090b6a7205d4dc9676
choihabin <154362206+haaaabin@users.noreply.github.com>
2024-02-22 15:03:52 +0900

    [Silver IV] Title: 상근이의 여행, Time: 148 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/9372. 상근이의 여행/상근이의 여행.py
+++ 백준/Silver/9372. 상근이의 여행/상근이의 여행.py
@@ -1,25 +1,9 @@
 import sys
 input = sys.stdin.readline
-sys.setrecursionlimit(10**9)
-
-def dfs(node, cnt):
-    vistied[node] = True
-    
-    for nodes in graph[node]:
-        if vistied[nodes] ==False:
-            cnt = dfs(nodes, cnt +1) 
-    return cnt
-
 T = int(input())
 
 for i in range(T):
-    N,M = map(int,input().split())
-    graph = [[] for i in range(N+10)]
-    vistied = [False] * (N+1)
-    
-    for i in range(M):
-        a,b = map(int,input().split())
-        graph[a].append(b)
-        graph[b].append(a)
-           
-    print(dfs(1,0))
+    n, m = map(int,input().split()) 
+    for i in range(m):
+        a, b = map(int,input().split())
+    print(n-1)
