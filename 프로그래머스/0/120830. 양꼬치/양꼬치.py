b53b98df1d4ca5a6c82768f673ac0a34f7a9386a
choihabin <154362206+haaaabin@users.noreply.github.com>
2024-02-23 00:09:18 +0900

    [level 0] Title: 양꼬치, Time: 0.00 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/0/120830. 양꼬치/양꼬치.py
+++ 프로그래머스/0/120830. 양꼬치/양꼬치.py
@@ -1,8 +1,3 @@
 def solution(n, k):
-    answer = 0
-    if n < 10:
-        answer = n * 12000 + k *2000
-    elif n >= 10:
-        s = k - (n //10)
-        answer = n * 12000 + s * 2000        
+    answer = n * 12000 + (k - (n//10)) * 2000  
     return answer
