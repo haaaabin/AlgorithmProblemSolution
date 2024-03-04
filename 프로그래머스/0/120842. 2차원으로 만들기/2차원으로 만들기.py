8ee2650d55de613f9e46847ac7d8c37ee2989284
choihabin <154362206+haaaabin@users.noreply.github.com>
2024-03-04 13:02:53 +0900

    [level 0] Title: 2차원으로 만들기, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/120842. 2차원으로 만들기/2차원으로 만들기.py
+++ 프로그래머스/0/120842. 2차원으로 만들기/2차원으로 만들기.py
@@ -1,6 +1,9 @@
 def solution(num_list, n):
     answer = []
-    for i in range(len(num_list) //n):
-        answer.append(num_list[n*i : n *(i+1)])
-    
+    temp = []
+    for num in num_list:
+        temp.append(num)
+        if len(temp) == n:
+            answer.append(temp)
+            temp = []
     return answer
