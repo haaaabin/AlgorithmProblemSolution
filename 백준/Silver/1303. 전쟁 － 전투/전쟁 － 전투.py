8e313e13ce70e9a6d3a922dd97bd6b94549cf134
choihabin <154362206+haaaabin@users.noreply.github.com>
2024-08-01 00:47:07 +0900

    [Silver I] Title: 전쟁 - 전투, Time: 64 ms, Memory: 34088 KB -BaekjoonHub

--- 백준/Silver/1303. 전쟁 － 전투/전쟁 － 전투.py
+++ 백준/Silver/1303. 전쟁 － 전투/전쟁 － 전투.py
@@ -2,36 +2,39 @@ import sys
 from collections import deque
 input = sys.stdin.readline
 
-N, M = map(int,input().split())
-peoples = [list(input().rstrip()) for _ in range(M)]
-
 dx = [-1,1,0,0]
 dy = [0,0,-1,1]
 
-def bfs(a,b,color):
-    q = deque()
-    q.append((a,b))
-    cnt = 1
-    peoples[a][b]= "."
-    while q:
-        x, y  = q.popleft()
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            
-            if(0 <= nx < M and 0 <= ny < N and peoples[nx][ny] == color):
-                peoples[nx][ny] = "."
-                q.append((nx,ny))
-                cnt += 1
-    return cnt    
+N,M = map(int,input().split())
+graph = [list(input().strip()) for _ in range(M)]
 
-w_result = 0
-b_result = 0
+def bfs(x,y,color):
+    q = deque()
+    q.append((x,y))
+    graph[x][y] = '.'
+    cnt = 1
+
+    while q:
+        x_, y_ = q.popleft()
+
+        for i in range(4):
+            nx = x_ + dx[i]
+            ny = y_ + dy[i]
+
+            if (0 <= nx < M and 0 <= ny < N and graph[nx][ny] == color):
+                q.append((nx,ny))
+                graph[nx][ny] = '.'
+                cnt += 1
+
+    return cnt
+
+W_result = 0
+B_result = 0
 for i in range(M):
     for j in range(N):
-        if peoples[i][j] == "W":
-            w_result += bfs(i,j,"W") **2
-        elif peoples[i][j] == "B":
-            b_result += bfs(i,j,"B") **2 
-            
-print(w_result, b_result)
+        if graph[i][j] == 'W':
+            W_result += bfs(i,j,'W') **2
+        elif graph[i][j] == 'B':
+            B_result += bfs(i,j,'B') **2
+
+print(W_result, B_result)
