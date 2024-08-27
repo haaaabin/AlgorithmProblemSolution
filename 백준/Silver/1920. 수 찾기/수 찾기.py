3c3092b9b6381ca123f96ea3e2ac96e0bf4c976d
choihabin <154362206+haaaabin@users.noreply.github.com>
2024-08-27 22:53:25 +0900

    [Silver IV] Title: 수 찾기, Time: 156 ms, Memory: 47324 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.py
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -1,27 +1,10 @@
 N = int(input())
-A = list(map(int,input().split()))
-
+A = set(map(int,input().split()))
 M = int(input())
-B = list(map(int,input().split()))
+B = map(int,input().split())
 
-A.sort()    # 1 2 3 4 5
-
-def search(target,data):
-    pl = 0
-    pr = N-1
-    
-    while pl <= pr:
-        pc = (pl+pr)//2
-        if target == data[pc]:
-            return True
-        elif target > data[pc]:
-            pl = pc+1
-        else:
-            pr = pc-1
-    return False
-
-for i in B:
-    if search(i,A):
-        print("1")
+for b in B:
+    if b in A:
+        print(1)
     else:
-        print("0")
+        print(0)
