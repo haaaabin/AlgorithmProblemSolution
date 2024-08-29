6b264ed1c8ddd16803f51f287654aba8a0c77043
choihabin <154362206+haaaabin@users.noreply.github.com>
2024-08-29 17:51:39 +0900

    [Silver IV] Title: 차집합, Time: 852 ms, Memory: 149976 KB -BaekjoonHub

---
+++ 백준/Silver/1822. 차집합/차집합.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+n, m = map(int,input().split())
+a = set(map(int,input().split()))
+b = set(map(int,input().split()))
+
+result = sorted(a-b)
+
+if result:
+    print(len(result))
+    print(*result)
+else:
+    print(0)
