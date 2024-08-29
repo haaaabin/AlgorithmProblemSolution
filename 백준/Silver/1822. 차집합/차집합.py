4408bbc743ef19901558e10ad8a806ae72be6b2f
choihabin <154362206+haaaabin@users.noreply.github.com>
2024-08-29 18:00:15 +0900

    [Silver IV] Title: 차집합, Time: 840 ms, Memory: 149968 KB -BaekjoonHub

--- 백준/Silver/1822. 차집합/차집합.py
+++ 백준/Silver/1822. 차집합/차집합.py
@@ -5,7 +5,7 @@ n, m = map(int,input().split())
 a = set(map(int,input().split()))
 b = set(map(int,input().split()))
 
-result = sorted(a-b)
+result = sorted(a.difference(b))
 
 if result:
     print(len(result))
