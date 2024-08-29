2d1dc96440d6156db3e21b4397fed86a77fc20ef
choihabin <154362206+haaaabin@users.noreply.github.com>
2024-08-29 16:19:00 +0900

    [Silver II] Title: 나무 자르기, Time: 3832 ms, Memory: 144340 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.py
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.py
@@ -4,18 +4,17 @@ trees = list(map(int,input().split()))
 start = 0
 end = max(trees)
 
-#이분탐색
 while start <= end:
-    mid = (start + end) //2
+    mid = (start+end) // 2
     total = 0
-    
+
     for tree in trees:
         if tree >= mid:
             total += tree - mid
-            
-    if total >= M:
-        start = mid +1
+        
+    if total >= M :
+        start = mid + 1
     else:
-        end = mid -1
+        end = mid - 1
 
 print(end)
