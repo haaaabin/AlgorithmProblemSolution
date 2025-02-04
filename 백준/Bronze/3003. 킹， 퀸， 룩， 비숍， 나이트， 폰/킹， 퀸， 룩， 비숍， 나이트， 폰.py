a4f32362f7d08955e0dd3aa1fb499340102ff931
choihabin <154362206+haaaabin@users.noreply.github.com>
2025-02-04 13:24:47 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.py
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.py
@@ -1,5 +1,5 @@
-n_list = [1, 1, 2, 2, 2, 8]
-y_list = list(map(int,input().split()))
+chess = [1,1,2,2,2,8]
+temp = list(map(int,input().split()))
 
 for i in range(6):
-    print(n_list[i] - y_list[i], end=' ')
+    print(chess[i] - temp[i], end =" ")
