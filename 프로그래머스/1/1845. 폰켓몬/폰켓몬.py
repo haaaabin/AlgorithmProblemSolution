d5fed0169e0f7ca7c4d9819be6fd636662053657
choihabin <154362206+haaaabin@users.noreply.github.com>
2025-04-05 17:02:13 +0900

    [level 1] Title: 폰켓몬, Time: 0.92 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
@@ -1,7 +1,7 @@
 def solution(nums):
     temp = list(set(nums))
     
-    if len(nums) // 2 < len(temp):
-        return len(nums) // 2
+    if len(nums) / 2 < len(temp):
+        return len(nums) / 2
     else:
         return len(temp)
