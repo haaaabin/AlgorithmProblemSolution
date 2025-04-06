30bc997dcea085786b93e3a68819ad664159fe7d
choihabin <154362206+haaaabin@users.noreply.github.com>
2025-04-07 02:19:48 +0900

    [Silver V] Title: 나이순 정렬, Time: 244 ms, Memory: 46020 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.py
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.py
@@ -3,12 +3,13 @@ input = sys.stdin.readline
 
 N = int(input())
 temp = []
+
 for i in range(N):
     age, name = map(str, input().split())
     age = int(age)
-    temp.append((age, name))
+    temp.append((age,name))
 
-temp.sort(key = lambda x : x[0])
+temp.sort(key=lambda x:x[0])
 
 for i in temp:
-    print(i[0], i[1])
+    print(i[0],i[1])
