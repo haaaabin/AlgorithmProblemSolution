c026e64c12504c0024f45a326c4e4a3840bf8c73
choihabin <154362206+haaaabin@users.noreply.github.com>
2025-04-07 19:47:00 +0900

    [Silver I] Title: 단지번호붙이기, Time: 56 ms, Memory: 34984 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -1,40 +1,45 @@
 from collections import deque
+import sys
+input = sys.stdin.readline
 
-N = int(input())
+n = int(input())
 
-graph = [list(map(int, input())) for _ in range(N)]
+graph = [list(map(int,input().strip())) for _ in range(n)]
+visited = [[False] * n for _ in range(n)]
 
-# 상하좌우
 dx = [-1,1,0,0]
 dy = [0,0,-1,1]
-    
-def bfs(graph,a,b):
-    queue = deque()
-    queue.append((a,b))
-    graph[a][b] = 0
+
+def bfs(x,y):
+    q = deque()
+    q.append((x,y))
+    visited[x][y] = True
     cnt = 1
 
-    
-    while queue:
-        x,y = queue.popleft()
-
+    while q:
+        x,y = q.popleft()
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
 
-            if nx < 0 or nx >= N or ny <0 or ny >= N:
-                continue
-
-            if graph[nx][ny] == 1:
-                graph[nx][ny] = 0
-                queue.append((nx,ny))
+            if 0 <= nx < n and 0 <= ny < n and graph[nx][ny] == 1 and not visited[nx][ny]:
+                q.append((nx,ny))
+                visited[nx][ny] = True
                 cnt += 1
+
     return cnt
 
-count = [bfs(graph, i, j) for i in range(N) for j in range(N) if graph[i][j] == 1]
+houses = []
+count = 0
 
-count.sort()
-print(len(count))
+for i in range(n):
+    for j in range(n):
+        if graph[i][j] == 1 and not visited[i][j]:
+            houses.append(bfs(i,j))
+            count += 1
 
-for i in range(len(count)):
-    print(count[i])
+print(count)
+
+houses.sort()
+for house in houses:
+    print(house)
