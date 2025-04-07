3e21e69d7a90f9232ca173933663d8df59ca2cd5
choihabin <154362206+haaaabin@users.noreply.github.com>
2025-04-07 15:20:50 +0900

    [Silver V] Title: 단어 정렬, Time: 544 ms, Memory: 37532 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.py
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.py
@@ -1,18 +1,9 @@
 N = int(input())
-words = []
 
-for _ in range(N):
-    words.append(input())
+words = [input() for _ in range(N)]
+temp = set(words)
+words = list(temp)
+words.sort(key=lambda x : (len(x) , x))
 
-# 중복 제거 위해 set 타입으로 변경
-# -> set을 list 타입으로 변경 
-words_list = set(words)
-words = list(words_list)
-
-# 오름차순 정렬
-words.sort()
-# 길이 순으로 정렬
-words.sort(key = len)
-
-for i in words:
-    print(i)
+for word in words:
+    print(word)
