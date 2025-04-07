da4c1bc30f947fd28773ed5b029d5decc55e306b
choihabin <154362206+haaaabin@users.noreply.github.com>
2025-04-07 18:23:47 +0900

    [Silver II] Title: 유기농 배추, Time: 64 ms, Memory: 34992 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.py
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -2,41 +2,41 @@ from collections import deque
 import sys
 input = sys.stdin.readline
 
-dx = [0, 1, 0, -1]
-dy = [1, 0,-1, 0]
+T = int(input())
 
 def bfs(x,y):
     q = deque()
     q.append((x,y))
-    baechu[x][y] = 0
-    
-    while q:
-        x_ , y_ = q.popleft()
+    visited[x][y] = 1
 
+    while q:
+        x,y = q.popleft()
+        
         for i in range(4):
-            nx = x_ + dx[i]
-            ny = y_ + dy[i]
-            
-            if nx < 0 or nx >= M or ny < 0 or ny >= N or baechu[nx][ny] == 0:
-                continue
-            
-            if baechu[nx][ny] ==1:
-                q.append((nx,ny))
-                baechu[nx][ny] = 0
-     
-T = int(input())
+            nx = x+dx[i]
+            ny = y+dy[i]
+
+            if 0 <= nx < m and 0 <= ny < n:
+                if graph[nx][ny] == 1 and not visited[nx][ny]:
+                    visited[nx][ny] = 1
+                    q.append((nx,ny))
+
 for _ in range(T):
-    M,N,K = map(int,input().split())
-    baechu = [[0] * N for _ in range(M)]
-    
-    for _ in range(K):
-        x,y = map(int,input().split())
-        baechu[x][y] = 1
-    
+    m,n,k = map(int,input().split())
+    graph = [[0] * n for _ in range(m)]
+    visited = [[0] * n for _ in range(m)]
+
+    for _ in range(k):
+        x,y= map(int,input().split())
+        graph[x][y]=1
+
+    dx = [-1,1,0,0]
+    dy = [0,0,-1,1]
+
     cnt = 0
-    for i in range(M):
-        for j in range(N):
-            if baechu[i][j] == 1:
-                  bfs(i,j)
-                  cnt +=1
-    print(cnt) 
+    for i in range(m):
+        for j in range(n):
+            if graph[i][j] == 1 and not visited[i][j]:
+                bfs(i,j)
+                cnt += 1
+    print(cnt)
