755dd2496843eb21af56df2087ce8744e532a1b8
choihabin <154362206+haaaabin@users.noreply.github.com>
2025-04-07 21:16:23 +0900

    [Gold V] Title: 토마토, Time: 1240 ms, Memory: 100420 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.py
+++ 백준/Gold/7576. 토마토/토마토.py
@@ -1,40 +1,39 @@
-import sys
 from collections import deque
+import sys
+input = sys.stdin.readline
 
-M , N = map(int,input().split())
-tomatos = []
-for _ in range(N):
-    tomatos.append(list(map(int,input().split())))
+m,n = map(int,input().split())
+graph = [list(map(int,input().split())) for _ in range(n)]
 
-dx = [-1,1,0,0,]
+dx = [-1,1,0,0]
 dy = [0,0,-1,1]
-       
+
 def bfs():
-    q = deque([])
+    q = deque()
     
-    for i in range(N):
-        for j in range(M):
-            if tomatos[i][j] == 1:
-                q.append([i,j])
+    for i in range(n):
+        for j in range(m):
+            if graph[i][j] == 1:
+                q.append((i,j))
 
     while q:
-        x , y = q.popleft()
+        x,y = q.popleft()
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
-            
-            if 0 <= nx < N and 0 <= ny < M and tomatos[nx][ny] == 0:
-                tomatos[nx][ny] = tomatos[x][y] + 1
-                q.append([nx,ny])
-       
-bfs()      
-     
+
+            if 0 <= nx < n and 0 <= ny < m and graph[nx][ny] == 0:
+                graph[nx][ny] = graph[x][y] + 1
+                q.append((nx,ny))
+
+bfs()
+
 day = 0
-for row in tomatos:
-    for toma in row:
-        if toma == 0:
+for row in graph:
+    for i in row:
+        if i == 0:
             print(-1)
             exit()
     day = max(day, max(row))
-    
-print(day - 1)
+
+print(day-1)
