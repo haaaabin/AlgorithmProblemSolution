2745d45f580cc0b6647e0917bdfde93244656dcd
choihabin <154362206+haaaabin@users.noreply.github.com>
2025-04-08 17:48:19 +0900

    [level 2] Title: 큰 수 만들기, Time: 119.19 ms, Memory: 11.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.py
@@ -0,0 +1,14 @@
+def solution(number, k):
+    stack = []
+    
+    for i in number:
+        while stack and k > 0 and stack[-1] < i:
+            stack.pop()
+            k -= 1
+        stack.append(i)
+    
+    if k > 0:
+        stack= stack[:-k]
+        
+    answer = ''.join(stack)
+    return answer
